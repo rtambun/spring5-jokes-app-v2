8bf6bcb5dd38ce01f4936d0ba9deeb088dca5b82
rtambun <richson.tambun@gmail.com>
2021-03-13 21:57:35 +0800

    Initial version of jokes app

---
+++ src/main/java/rtambun/training/spring/JokeApp/JokeAppApplication.java
@@ -0,0 +1,13 @@
+package rtambun.training.spring.JokeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeAppApplication.class, args);
+	}
+
+}
